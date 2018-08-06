9a2722deb9da926aae25018743854e5b63938248
Ruzanna94 <33309355+Ruzanna94@users.noreply.github.com>
2018-08-06 17:59:54 +0400

    Delete AbstractFactory.java

--- AbstractFactory.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by ruzanna.hovsepyan on 8/6/2018.
- */
-public interface AbstractFactory {
-     AbstractTypeScriptFile createTypeScriptFiles();
-     AbstractHtmlFile createHtmlScriptFiles();
-     AbstarctCssFile createCssFiles();
-
-}
