081d603a2ca3506a378dbb493ba31cca021364f7
TatevikArakelyan <40390315+TatevikArakelyan@users.noreply.github.com>
2018-08-17 09:18:50 +0400

    Update FactoryMethodApp.java

--- Tatevik.Arakelyan/factoryMethod/FactoryMethodApp.java
+++ Tatevik.Arakelyan/factoryMethod/FactoryMethodApp.java
@@ -1,64 +1,16 @@
-package factoryMethod;
+
+import impl.SportsCarFactory;
+import interfaces.SportsCar;
+import util.CarsUtil;
+
+import java.util.List;
 
 /**
- * @author by Tatevik.Arakelyan on 8/7/2018.
+ * @author by Tatevik.Arakelyan on 8/16/2018.
  */
 public class FactoryMethodApp {
 	public static void main(String[] args) {
-
-		//CarMaker maker = new SportsCarMaker(); // new TrucksCarMaker()
-
-		CarMaker maker = getMakerByName("TrucksCar");
-		Car car = maker.createCar();
-		car.carsMark();
-	}
-
-	public static CarMaker getMakerByName(String maker) {
-		switch (maker) {
-			case "SportsCar":
-				return new SportsCarMaker();
-			case "TrucksCar":
-				return new TrucksCarMaker();
-			default:
-				throw new RuntimeException("Incorrect mark of a car" + maker);
-		}
-	}
-}
-
-interface Car {
-	void carsMark();
-}
-
-class SportsCar implements Car {
-	@Override
-	public void carsMark() {
-		System.out.println("It's a sports car");
-	}
-}
-
-class TrucksCar implements Car {
-	@Override
-	public void carsMark() {
-		System.out.println("It's a truck car");
-	}
-}
-
-interface CarMaker {
-	Car createCar();
-}
-
-class SportsCarMaker implements CarMaker {
-
-	@Override
-	public Car createCar() {
-		return new SportsCar();
-	}
-}
-
-class TrucksCarMaker implements CarMaker {
-
-	@Override
-	public Car createCar() {
-		return new TrucksCar();
+		List<SportsCar> sportsCar = CarsUtil.createCars(new SportsCarFactory(), "Bugatti", 10);
+		sportsCar.get(0).carsMark();
 	}
 }
