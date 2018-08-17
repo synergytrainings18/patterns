c1171714a528bae02c2640156625ed80db6e74a8
TatevikArakelyan <40390315+TatevikArakelyan@users.noreply.github.com>
2018-08-17 09:16:31 +0400

    Update AbstractFactoryApp.java

--- Tatevik.Arakelyan/abstractFactory/AbstractFactoryApp.java
+++ Tatevik.Arakelyan/abstractFactory/AbstractFactoryApp.java
@@ -1,103 +1,12 @@
-package abstractFactory;
+
+import impl.LeftWandFactory;
+import util.TwixUtil;
 
 /**
- * @author by Tatevik.Arakelyan on 8/7/2018.
+ * @author by Tatevik.Arakelyan on 8/16/2018.
  */
 public class AbstractFactoryApp {
-
 	public static void main(String[] args) {
-
-		TwixFactory factory = getFactoryBySideOfWand("Right");
-		Caramel caramel = factory.getCaramel();
-		Chocolate chocolate = factory.getChocolate();
-
-		caramel.kindOfPutOnCaramel();
-		chocolate.kindOfPutOnChocolate();
-
-	}
-
-	private static TwixFactory getFactoryBySideOfWand(String side) {
-		switch (side) {
-			case "Right":
-				return new RightWandFactory();
-			case "Left":
-				return new LeftWandFactory();
-			default:
-				throw new RuntimeException("Can't get factory for this side:" + side);
-		}
+		TwixUtil.createTwix(new LeftWandFactory());
 	}
 }
-
-interface TwixFactory {
-
-	Chocolate getChocolate();
-
-	Caramel getCaramel();
-}
-
-class RightWandFactory implements TwixFactory {
-
-	@Override
-	public Chocolate getChocolate() {
-		return new RightChocolate();
-	}
-
-	@Override
-	public Caramel getCaramel() {
-		return new RightCaramel();
-	}
-}
-
-class LeftWandFactory implements TwixFactory {
-
-	@Override
-	public Chocolate getChocolate() {
-		return new LeftChocolate();
-	}
-
-	@Override
-	public Caramel getCaramel() {
-		return new LeftCaramel();
-	}
-}
-
-interface Chocolate {
-	void kindOfPutOnChocolate();
-}
-
-interface Caramel {
-	void kindOfPutOnCaramel();
-}
-
-class RightChocolate implements Chocolate {
-	@Override
-	public void kindOfPutOnChocolate() {
-		System.out.println("Dip a wand into chocolate.");
-	}
-}
-
-class RightCaramel implements Caramel {
-
-	@Override
-	public void kindOfPutOnCaramel() {
-		System.out.println("Put a caramel vertically in a wand.");
-	}
-}
-
-class LeftChocolate implements Chocolate {
-	@Override
-	public void kindOfPutOnChocolate() {
-		System.out.println("Put a chocolate a kind of top to down  in a wand․");
-	}
-}
-
-class LeftCaramel implements Caramel {
-
-	@Override
-	public void kindOfPutOnCaramel() {
-		System.out.println("Put a caramel like a waves in a wand.");
-	}
-}
-
-
-
