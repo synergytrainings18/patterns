f3f30a227bc11e0a55d96f06119a73855a4ace69
Ruzanna94 <33309355+Ruzanna94@users.noreply.github.com>
2018-08-27 09:35:11 +0400

    Add files via upload

---
+++ proxy/IvoryTower.java
@@ -0,0 +1,13 @@
+package proxy;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class IvoryTower implements WizardTower {
+    private static final Logger LOGGER = LoggerFactory.getLogger(IvoryTower.class);
+    @Override
+    public void enter(Wizard wizard) {
+        LOGGER.info("{} enters the tower.", wizard);
+
+    }
+}
---
+++ proxy/Wizard.java
@@ -0,0 +1,15 @@
+package proxy;
+
+public class Wizard {
+
+    private final String name;
+
+    public Wizard(String name) {
+        this.name = name;
+    }
+
+    @Override
+    public String toString() {
+        return name;
+    }
+}
