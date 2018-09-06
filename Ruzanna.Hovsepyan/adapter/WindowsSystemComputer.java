2173757e934926397d6189707453e298c4cccf2a
Ruzanna94 <33309355+Ruzanna94@users.noreply.github.com>
2018-09-06 10:15:49 +0400

    Delete WindowsSystemComputer.java

--- Ruzanna.Hovsepyan/adapter/WindowsSystemComputer.java
+++
@@ -1,9 +0,0 @@
-/**
- * Created by ruzanna.hovsepyan on 9/3/2018.
- */
-public class WindowsSystemComputer implements Computer {
-    @Override
-    public void readData() {
-        //read Data with windows system
-    }
-}
